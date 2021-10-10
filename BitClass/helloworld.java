5ca361ab08b252b22b00f7a80ce32761e2243667
wfm1021 <810760487@qq.com>
2021-10-10 17:04:48 +0800

    fisrt commit

---
+++ BitClass/helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld{
+      public static void main(String [] args){
+            System.out.println("Hello World");
+            int a=10;
+            System.out.println(a);
+            short b=10;
+            System.out.println(b);
+            double num = 1.1;
+            System.out.println(num * num);
+      }
+}
