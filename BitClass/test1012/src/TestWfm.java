c553077704cd718d3d14302335397d1b3bb92568
wfm1021 <810760487@qq.com>
2021-10-12 22:04:33 +0800

    20211012

---
+++ BitClass/test1012/src/TestWfm.java
@@ -0,0 +1,11 @@
+public class TestWfm {
+    public static void main(String[] args) {
+        int a=25;
+        int b=22;
+        int c=0xf;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(~a);
+        System.out.println(a^b);
+    }
+}
