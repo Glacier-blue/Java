86a8c278f63a80b1ed561c1c6c006d04419cf4e9
wfm1021 <810760487@qq.com>
2021-10-13 20:18:08 +0800

    check

--- BitClass/test1012/src/TestWfm.java
+++ BitClass/test1012/src/TestWfm.java
@@ -2,7 +2,6 @@ public class TestWfm {
     public static void main(String[] args) {
         int a=25;
         int b=22;
-        int c=0xf;
         System.out.println(a&b);
         System.out.println(a|b);
         System.out.println(~a);
