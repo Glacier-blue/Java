9c706230f9c04a2564f8126f1ced07d3145810fe
wfm1021 <810760487@qq.com>
2021-11-10 21:27:02 +0800

    20211101

--- BitClass/test1110/src/Main.java
+++ BitClass/test1110/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+            int m=1;
+            int n=30;
+            while(n>0){
+                int a=m<<n;
+                n--;
+        }
     }
 }
--- BitClass/test1110/src/com/bit/demo/Main.java
+++ BitClass/test1110/src/com/bit/demo/Main.java
@@ -1,4 +1,3 @@
 package com.bit.demo;
 
-public class Main {
-}
+
