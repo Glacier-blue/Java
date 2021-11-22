117d4c0f0ff7e35fb4410e00b8f89916c7b5d9e7
wfm1021 <810760487@qq.com>
2021-11-22 19:17:36 +0800

    del

--- BitClass/pratice/src/data1115/Shape.java
+++
@@ -1,7 +0,0 @@
-package data1115;
-
- public class Shape {
-     public void draw(){
-
-     }
-}
--- BitClass/pratice/src/data1120/Main.java
+++
@@ -1,7 +0,0 @@
-package data1120;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(10);
-    }
-}
