2011d290b12c82c552f06c651ce2cbb51ed529ef
wfm1021 <810760487@qq.com>
2021-11-24 22:57:46 +0800

    9999

--- BitClass/pratice/src/day1124/Main.java
+++ BitClass/pratice/src/day1124/Main.java
@@ -2,6 +2,11 @@ package day1124;
 
 import java.util.Scanner;
 
+/**
+ * 1、引用类型和整数类型无法公用判断相等的体系
+ * 2、getpos返回值若为泛型，无法处理异常情况
+ */
+
 public class Main {
     public static void main(String[] args) {
         MyArrayList<String> a=new MyArrayList<String>();
