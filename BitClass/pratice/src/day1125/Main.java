99db824f2921e1206b91f3ee9f586661311dee8c
wfm1021 <810760487@qq.com>
2021-11-25 22:29:38 +0800

    20211125

--- BitClass/pratice/src/day1125/Main.java
+++ BitClass/pratice/src/day1125/Main.java
@@ -1,8 +1,20 @@
 package day1125;
 
+class A{
+    public static void func1(){
+        System.out.println("静态方法不用对象可以调用");
+    }
+    public static int num=10;
+}
 public class Main {
-
     public static void main(String[] args) {
+        A.func1();
+        System.out.println(A.num);
+    }
+    public static void main2(String[] args) {
+        final int a=10;
+    }
+    public static void main1(String[] args) {
         Main main=new Main();
         main.fun1();
 
