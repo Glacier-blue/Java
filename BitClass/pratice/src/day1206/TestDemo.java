d228438b95ad1e372b5ac971f698a276ac062098
wfm1021 <810760487@qq.com>
2021-12-06 15:57:45 +0800

    20211106

---
+++ BitClass/pratice/src/day1206/TestDemo.java
@@ -0,0 +1,9 @@
+package day1206;
+
+public class TestDemo{
+    public static void main(String[] args)throws CloneNotSupportedException {
+        TestStu student1=new TestStu("zhangsan","0001",90.78);
+        TestStu student2=(TestStu) student1.clone();
+        System.out.println(student2);
+    }
+}
