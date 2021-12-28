f1336d44177bd5f6dce190cdbec48e1e732a03d8
Glacier-blue <810760487@qq.com>
2021-12-28 19:26:39 +0800

    测试compareTo和字符串和字符转化

--- BitClass/JavaSE/src/TestString.java
+++ BitClass/JavaSE/src/TestString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TestString {
     static final String str1="123";
     static final String str2;
@@ -6,7 +8,7 @@ public class TestString {
         s=str1+str2;
     }
     static String s;
-    public static void main(String[] args) {
+    public static void main3(String[] args) {
         System.out.println(s=="123456");
         {
             String s=str1+"456";
@@ -16,16 +18,24 @@ public class TestString {
             String s="123"+str2;
             System.out.println(s=="123456");
         }
-        {
-            String s=str1+str2;
-            System.out.println(s=="123456");
-        }
     }
 
+    public static void main(String[] args) {
+        char[] chars={'a','b','c','d'};
+        String str=new String(chars);
+        System.out.println(str);
+        char[] chars1=str.toCharArray();
+        System.out.println(Arrays.toString(chars1));
+    }
+    /**
+     * 测试：compareTo函数
+     * s1.compareTo(s2)
+     * s1-s2每个字符向后减
+     */
     public static void main1(String[] args) {
-        final String s1="123";
-        final String s2="456";
-        String s3=s1+s2;
-        System.out.println(s3=="123456");
+        String s1="abcdefg";
+        String s2="bcdefg";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s1));
     }
 }
