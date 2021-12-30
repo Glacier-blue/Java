22541bd8d4cd0b3f559feb0534a0ec83f201155a
Glacier-blue <810760487@qq.com>
2021-12-30 10:55:58 +0800

    类和面向对象；多态和几次

--- BitClass/JavaSE/src/TestClass.java
+++ BitClass/JavaSE/src/TestClass.java
@@ -1,2 +1,48 @@
+
+interface Fly{
+     abstract public void fly();
+}
+abstract class Biology{
+    public int age;
+    abstract public void grow();
+}
+class Animal extends Biology{
+    public String name;
+    public void eat(){
+        System.out.println("已经进食");
+    }
+
+    @Override
+    public void grow() {
+        System.out.println("成长");
+    }
+
+    public Animal(String name){
+        this.name=name;
+    }
+}
+class Bird extends Animal implements Fly{
+    public int flyAble;
+    public Bird(String name,int flyAble){
+        super(name);
+        this.flyAble=flyAble;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("用喙进食");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("飞起来了哦");
+    }
+}
 public class TestClass {
+    public static void main(String[] args) {
+        Animal huihui=new Bird("huihui",3);
+        huihui.eat();
+        Bird dingding=new Bird("dingding",5);
+        dingding.fly();
+    }
 }
