27bf0cada1f8385ed7c62558243aa04feea0dac7
Glacier-blue <810760487@qq.com>
2022-01-01 22:22:36 +0800

    类和对象测试

--- BitClass/JavaSE/src/TestClass.java
+++ BitClass/JavaSE/src/TestClass.java
@@ -1,48 +1,67 @@
 
-interface Fly{
-     abstract public void fly();
-}
-abstract class Biology{
-    public int age;
-    abstract public void grow();
-}
-class Animal extends Biology{
+class Animal {
     public String name;
-    public void eat(){
-        System.out.println("已经进食");
+    public Animal(String name) {
+        this.name = name;
     }
-
-    @Override
-    public void grow() {
-        System.out.println("成长");
-    }
-
-    public Animal(String name){
-        this.name=name;
+    public void eat(String food) {
+        System.out.println(this.name + "正在吃" + food);
     }
 }
-class Bird extends Animal implements Fly{
-    public int flyAble;
-    public Bird(String name,int flyAble){
+
+class Cat extends Animal {
+    public Cat(String name) {
+        // 使用 super 调用父类的构造方法.
         super(name);
-        this.flyAble=flyAble;
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("用喙进食");
-    }
-
-    @Override
-    public void fly() {
-        System.out.println("飞起来了哦");
     }
 }
+
+class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
+    public void fly() {
+        System.out.println(this.name + "正在飞 ︿(￣︶￣)︿");
+    }
+}
+class Person{
+    public String name;
+    public int age;
+    public void eat(){
+        System.out.println(name+" is eat");
+    }//方法
+    public static int count;//静态变量--属于类
+}
+
 public class TestClass {
     public static void main(String[] args) {
-        Animal huihui=new Bird("huihui",3);
-        huihui.eat();
-        Bird dingding=new Bird("dingding",5);
-        dingding.fly();
+        Cat cat = new Cat("小黑");
+        cat.eat("猫粮");
+        Bird bird = new Bird("圆圆");
+        bird.fly();
+    }
+    public static void main3(String[] args) {
+        Person play1=new Person();
+        Person play2=new Person();
+        play1.count=1;
+        play2.count=2;
+        System.out.println(play1.count);
+        System.out.println(play2.count);
+    }
+    public static void main2(String[] args) {
+        Person play1=new Person();
+        play1.name="number1";
+        play1.age=18;
+        Person play2=new Person();
+        play2.name="number2";
+        play2.age=19;
+        System.out.println("名字:"+play1.name+" 年龄:"+play1.age);
+        System.out.println("名字:"+play2.name+" 年龄:"+play2.age);
+    }
+    public static void main1(String[] args) {
+        Person play1=new Person();//类实例化为对象
+        System.out.println(play1.name);
+        System.out.println(play1.age);
+        play1.eat();
     }
 }
