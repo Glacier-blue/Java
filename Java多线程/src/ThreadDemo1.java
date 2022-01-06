727d01f27ab689658a3b4ca511ff96626a06e07b
Glacier-blue <810760487@qq.com>
2022-01-06 13:30:13 +0800

    first commit

--- Java多线程/src/ThreadDemo1.java
+++ Java多线程/src/ThreadDemo1.java
@@ -2,63 +2,6 @@
 
 public class ThreadDemo1 {
 
-    private long count=100_0000_0000L;
-    public static void main2(String[] args) throws InterruptedException {
-        ThreadDemo1 threadDemo1=new ThreadDemo1();
-        long start=System.currentTimeMillis();
-        threadDemo1.serival();
-        long end=System.currentTimeMillis();
-        System.out.println(end-start);
-        threadDemo1.concurrency();
-        // 执行时间由11s->3s
-    }
-
-    public  void serival(){
-        long a=0;
-        for(long i=0;i<count;i++){
-            a++;
-        }
-        long b=0;
-        for(long i=0;i<count;i++){
-            b++;
-        }
-    }
-
-    /**
-     * t1，t2和main都是并发执行的
-     */
-    public void concurrency() throws InterruptedException {
-        long start=System.currentTimeMillis();
-        //匿名内部类
-        Thread thread1=new MyThread(){
-            @Override
-            public void run() {
-                long a=0;
-                for(long i=0;i<count;i++){
-                    a++;
-                }
-            }
-        };
-        Thread thread2=new MyThread(){
-            @Override
-            public void run() {
-                long a=0;
-                for(long i=0;i<count;i++){
-                    a++;
-                }
-            }
-        };
-        thread1.start();
-        thread2.start();
-        thread1.join();
-        thread2.join();
-        long end=System.currentTimeMillis();
-        System.out.println(end-start);
-    }
-
-/**
- * ---------------------------------------分割线--------------------------------------------------------------------
- */
     static class MyThread extends Thread{
         @Override
         public void run() {
@@ -70,7 +13,7 @@ public class ThreadDemo1 {
      * 还需要指定给线程指定要执行那些代码
      * 这里使用直接继承Thread类的方法
      */
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         //当 Thread 对象被创建出来的时候, 内核中并没有随之产生一个线程(PCB).
         Thread myThread=new MyThread();
         //调用start后，代码执行后内核创建（线程）PCB
