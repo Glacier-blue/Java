2583751fc8f37f481e6269f8468b2310a74661d2
Glacier-blue <810760487@qq.com>
2022-01-10 19:42:30 +0800

    贪心算法

--- Algorithm/src/GreedyAlgorithm.java
+++ Algorithm/src/GreedyAlgorithm.java
@@ -1,9 +1,39 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * 贪心算法
  */
 public class GreedyAlgorithm {
+
+
+    /**
+     *
+     * @param activity 二维数组，表示活动的开始和截至时间
+     * @return 表示一天可以进行活动的最大数
+     * * 先结束的活动后可以进行更多的活动
+     */
+
+    public int maxActivitySelector(int[][] activity){
+        /**
+         * 按照活动的截至之间进行排序
+         */
+        Arrays.sort(activity, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        });
+        int i=0;
+        int ans=1;
+        for(int j=1;j<activity.length;j++){
+            if(activity[j][0]>=activity[i][1]){
+                i=j;
+                ans++;
+            }
+        }
+        return ans;
+    }
 
     /**
      * 工厂调度问题
@@ -38,7 +68,7 @@ public class GreedyAlgorithm {
         return maxTime;
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int[] taskTime={1,3,4,6,8,3};
         int m=4;
         int ans=minTime(taskTime,m);
