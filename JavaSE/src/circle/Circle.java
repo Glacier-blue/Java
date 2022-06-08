039cefdcf49a84b6411056579001b0f8b5465bcf
Minria <810760487@qq.com>
2022-06-08 22:59:55 +0800

    while和do while

---
+++ JavaSE/src/circle/Circle.java
@@ -0,0 +1,16 @@
+package circle;
+
+public class Circle {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        while(num1<0){
+            System.out.println("num1:"+num1);
+            num1++;
+        }
+        do {
+            System.out.println("num2:"+num2);
+            num2++;
+        }while(num2<0);
+    }
+}
