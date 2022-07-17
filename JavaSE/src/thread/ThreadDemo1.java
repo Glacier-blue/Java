c699eb11c14e1af96492a5765fb12a5f80c46001
Minria <810760487@qq.com>
2022-07-17 22:57:54 +0800

    新增Callable和线程池

--- JavaSE/src/thread/ThreadDemo1.java
+++ JavaSE/src/thread/ThreadDemo1.java
@@ -1,13 +1,51 @@
 package thread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class ThreadDemo1 {
+
+    // 通过Callable和线程池
+    public static class MyCallable implements Callable<String>{
+        private String name;
+        public MyCallable(String name){
+            this.name = name;
+        }
+        public MyCallable(){}
+
+        @Override
+        public String call() throws Exception {
+            return name;
+        }
+    }
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        List<Future> list = new ArrayList<>();
+        for(int i = 0 ;i < 5; i++){
+            Callable c = new MyCallable("i:"+i);
+            Future future = pool.submit(c);
+            System.out.println("submit a callable thread:" + i);
+            list.add(future);
+        }
+        pool.shutdown();
+        for(Future future:list){
+            try {
+                System.out.println("get the result from callable thread:"+future.get().toString());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     static class MyRunnable implements Runnable {
         @Override
         public void run() {
             System.out.println("我是一个新线程");
         }
     }
-    public static void main(String[] args) {
+    public static void main5(String[] args) {
         Thread t=new Thread(()-> System.out.println("这是一个新线程"));
         t.start();
     }
